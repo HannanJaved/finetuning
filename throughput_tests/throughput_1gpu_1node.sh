d768acf9966e00911e173b1f2b55b2c22936c06f
HannanJaved <hannanm@outlook.de>
2026-03-28 09:40:16 +0100

    added scripts for throughput testing on capella for Qwen3 1.7B

---
+++ throughput_tests/throughput_1gpu_1node.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#SBATCH --job-name=sft_tp_1g1n
+#SBATCH --output=/data/cat/ws/hama901h-Posttraining/.logs/Qwen3/throughput/%x_%j.out
+#SBATCH --error=/data/cat/ws/hama901h-Posttraining/.logs/Qwen3/throughput/%x_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=16G
+#SBATCH --time=02:00:00
+#SBATCH --partition=capella
+
+echo "JOB NAME" $SLURM_JOB_NAME
+
+module load CUDA
+source /data/horse/ws/hama901h-BFTranslation/venv-TRL/bin/activate
+
+export HF_HOME="/data/cat/ws/hama901h-Posttraining/.cache"
+export HF_DATASETS_CACHE="/data/cat/ws/hama901h-Posttraining/.cache"
+export PYTHONPATH="/data/horse/ws/hama901h-BFTranslation/venv-TRL/lib/python3.11/site-packages"
+
+export MASTER_PORT=$(shuf -i 20000-29999 -n 1)
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+export LOCAL_RANK=$SLURM_LOCALID
+export RANK=$SLURM_PROCID
+GPUS_PER_NODE=1
+export WORLD_SIZE=$((GPUS_PER_NODE*SLURM_NNODES))
+
+cd /data/cat/ws/hama901h-Posttraining/finetuning/alignment-handbook/
+ACCELERATE_CONFIG_FILE=/data/cat/ws/hama901h-Posttraining/finetuning/qwen3/zero3_1gpu.yaml
+CONFIG_FILE=/data/cat/ws/hama901h-Posttraining/finetuning/throughput_tests/config_throughput_1gpu_1node.yaml
+
+echo "JOBNAME" $SLURM_JOB_NAME
+echo "CONFIG" $CONFIG_FILE
+pwd -P
+
+export CMD="scripts/sft.py --config $CONFIG_FILE"
+
+SRUN_ARGS=" \
+    --wait=60 \
+    --kill-on-bad-exit=1 \
+    "
+
+export ACC_LAUNCHER="accelerate launch \
+    --rdzv_conf \"rdzv_backend=c10d,rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT\" \
+    --config_file $ACCELERATE_CONFIG_FILE \
+    --num_machines $SLURM_NNODES \
+    --num_processes $WORLD_SIZE \
+    --main_process_ip $MASTER_ADDR \
+    --main_process_port $MASTER_PORT \
+    --machine_rank \$SLURM_PROCID \
+    --role \$(hostname -s|tr -dc '0-9'): \
+    --tee 3 \
+    "
+
+srun $SRUN_ARGS --jobid $SLURM_JOB_ID bash -c "$ACC_LAUNCHER --role \$SLURMD_NODENAME: $CMD"
+
+echo "END TIME: $(date)"
+echo "END $SLURM_JOBID: $(date)"
